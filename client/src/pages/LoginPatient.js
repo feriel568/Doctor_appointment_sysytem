95f1d46c26a46fc17eea099ed80607329c1eaf63
ferielRabah <ferielrabah1@gmail.com>
2024-02-23 14:34:46 +0100

    Add patient's login page

--- client/src/pages/LoginPatient.js
+++ client/src/pages/LoginPatient.js
@@ -3,9 +3,40 @@ import '../Styles/loginPatient.css'
 
 const LoginPatient = () => {
   return (
-   <div>
-    
-   </div>
+    <div className="loginContainerPt">
+    <div className="loginFormPt">
+      <h2>Login</h2>
+      <div className="inputContainerPt">
+        <input
+          // value={username}
+          placeholder="Enter your username here"
+          // onChange={(ev) => setUsername(ev.target.value)}
+          className="inputBox"
+        />
+      </div>
+      <div className="inputContainerPt">
+        <input
+          type="password"
+          // value={password}
+          placeholder="Enter your password here"
+          // onChange={(ev) => setPassword(ev.target.value)}
+          className="inputBox"
+        />
+        <div className='fPass'>
+          <p>Forget password</p>          
+         </div>
+      </div>
+      <div className="inputContainerPt">
+        {/* <p className="error-message">{error}</p> */}
+        <input
+          className="inputButtonPt"
+          type="button"
+          // onClick={handleLogin}
+          value="Login"
+        />
+      </div>
+    </div>
+  </div>
   );
 };
 
